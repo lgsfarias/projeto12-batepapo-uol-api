67f8ac529f5ad32adc1799145a93ebb484c05bcb
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-04-28 07:31:43 -0300

    Import dotenv and use environment variable

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
 import express, { json } from 'express';
 import { MongoClient } from 'mongodb';
+import cors from 'cors';
+import chalk from 'chalk';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 app.use(json());
+app.use(cors());
 
-const mongoClient = new MongoClient('mongodb://localhost:27017');
+const MONGO_URI = process.env.MONGO_URI;
+const mongoClient = new MongoClient(MONGO_URI);
 let db;
 
 mongoClient.connect().then(() => {
     db = mongoClient.db('batepapo-uol-api');
 });
 
-app.listen(5000, () => {
-    console.log('Servidor rodando na porta 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(chalk.green(`Server running on port ${PORT}`));
 });
